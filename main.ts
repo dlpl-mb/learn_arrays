bd5ccdc5f59bb40f8fede0f32d8014164b4b5d94
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2021-09-18 13:24:23 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,16 @@
-let alter = ""
-let name = ""
-let namens_liste = ["Maria", "Georg", "Sophie"]
-let Alters_liste = ["12", "11", "12"]
-basic.forever(function () {
-    for (let Index = 0; Index <= 2; Index++) {
-        name = namens_liste[Index]
-        alter = Alters_liste[Index]
+input.onButtonPressed(Button.A, function () {
+    anzahl_elemente = namens_liste.length
+    for (let index = 0; index <= anzahl_elemente - 1; index++) {
+        name = namens_liste[index]
+        alter = alters_liste[index]
         basic.showString("" + name + " ist " + alter + "Jahre alt")
         basic.pause(5000)
     }
 })
+let alter = ""
+let name = ""
+let anzahl_elemente = 0
+let alters_liste: string[] = []
+let namens_liste: string[] = []
+namens_liste = ["Maria", "Georg", "Sophie"]
+alters_liste = ["12", "11", "12"]
